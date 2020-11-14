8a5de6546ae857236d0d68a7034ad90b37669178
Stephen Eisen <stephen.eisen27@gmail.com>
2020-11-14 17:21:10 -0500

    Fixed navbar position, removed bootstrap

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Projects.css";
+import image1 from "../../images/astar.png";
 
 const ProjectCard = ({ project }) => {
 
@@ -8,7 +9,7 @@ const ProjectCard = ({ project }) => {
     <Link to={ project.url }>
       <li className="project-card" key={project.id}>
           {/* <img className="projects-thumbnail" src={ require(project.thumbnail+"") } alt="projects-thumbnail"/> */}
-          <img className="projects-thumbnail" src="./images/astar.png" alt="pp"/>
+          <img className="projects-thumbnail" src={image1} alt="pp"/>
           <h1 className="projects-title">{ project.title }</h1>
 
           <div style={{ display: 'flex', marginTop: 5 }}>
@@ -17,7 +18,7 @@ const ProjectCard = ({ project }) => {
      
           <p>{ project.info }</p>
       </li>
-    </Link>
+    </Link> 
   )
 }
 
